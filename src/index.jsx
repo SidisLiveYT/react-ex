b20e3fbd1cca388487a70a9cf099b5dd2d308a43
sidisliveyt <sidisliveytofficial@gmail.com>
2022-04-13 15:12:10 +0530

    chore: Boiler Plate

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const rootReact = ReactDOM.createRoot(document.getElementById('root'));
+rootReact.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
